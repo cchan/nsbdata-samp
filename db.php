a493d00e6e46b4259dec60762c8bfde19739c9d1
Clive Chan <cc@clive.io>
2019-03-09 01:10:11 -0500

    heroku cleardb

--- db.php
+++ db.php
@@ -1,6 +1,13 @@
 <?php
 require_once 'meekrodb.2.3.class.php';
-DB::$user = 'root';
-DB::$password = '';
-DB::$dbName = 'nsbdata';
+
+$dburl = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
+DB::$host = $dburl['host'];
+DB::$user = $dburl['user'];
+DB::$password = $dburl['pass'];
+DB::$dbName = ltrim('/', $dburl['path']);
+if (array_key_exists('port', $dburl)) {
+  DB::$port = $dburl['port'];
+}
 ?>
