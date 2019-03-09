439aab8ddfcc0de57bd28bd50628dec8f260ed7f
Clive Chan <cc@clive.io>
2019-03-09 01:14:40 -0500

    update based on https://devcenter.heroku.com/articles/cleardb#using-cleardb-with-php

--- db.php
+++ db.php
@@ -6,8 +6,5 @@ $dburl = parse_url(getenv('CLEARDB_DATABASE_URL'));
 DB::$host = $dburl['host'];
 DB::$user = $dburl['user'];
 DB::$password = $dburl['pass'];
-DB::$dbName = ltrim('/', $dburl['path']);
-if (array_key_exists('port', $dburl)) {
-  DB::$port = $dburl['port'];
-}
+DB::$dbName = substr($dburl["path"], 1);
 ?>
